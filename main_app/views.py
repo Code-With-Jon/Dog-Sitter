897d3d94df4c397fe9bb5aa5f943837375de4491
Jon <jon@Sciences-MacBook-Pro.local>
2020-01-05 14:52:32 -0800

    wrapped up few edits

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 # Add UdpateView & DeleteView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Add the following import
 from django.http import HttpResponse
 from .models import Dog, Toy, Photo
@@ -21,12 +25,12 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-
+@login_required
 def dogs_index(request):
-    dog = Dog.objects.all()
+    dog = Dog.objects.filter(user=request.user)
     return render(request, 'dogs/index.html', {'dog': dog})
 
-
+@login_required
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     # instantiate FeedingForm to be rendered in the template
@@ -39,7 +43,7 @@ def dogs_detail(request, dog_id):
 
 # add this new function below dogs_detail
 
-
+@login_required
 def add_feeding(request, dog_id):
     # create the ModelForm using the data in request.POST
     form = FeedingForm(request.POST)
@@ -52,46 +56,74 @@ def add_feeding(request, dog_id):
         new_feeding.save()
     return redirect('detail', dog_id=dog_id)
 
+@login_required
 def assoc_toy(request, dog_id, toy_id):
   Dog.objects.get(id=dog_id).toys.add(toy_id)
   return redirect('detail', dog_id=dog_id)
 
+@login_required
 def unassoc_toy(request, dog_id, toy_id):
   Dog.objects.get(id=dog_id).toys.remove(toy_id)
   return redirect('detail', dog_id=dog_id)
 
-class DogCreate(CreateView):
+class DogCreate(LoginRequiredMixin, CreateView):
     model = Dog
     fields = '__all__'
+# This inherited method is called when a
+  # valid cat form is being submitted
+    def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+      form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+      return super().form_valid(form)
 
-
-class DogUpdate(UpdateView):
+class DogUpdate(LoginRequiredMixin, UpdateView):
     model = Dog
     fields = ['breed', 'description', 'age']
 
 
-class DogDelete(DeleteView):
+class DogDelete(LoginRequiredMixin, DeleteView):
     model = Dog
     success_url = '/dogs/'
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys/'
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+@login_required
 def add_photo(request, dog_id):
   S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
   BUCKET = 'dog-sitter'
